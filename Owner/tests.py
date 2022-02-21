02c1147b3d28ef2c13aae82c7d3e0537bdfa9f1f
shubham sapkota <shubham.sapkota1@gmail.com>
2022-02-21 21:29:40 +0545

    done

--- Owner/tests.py
+++ Owner/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from Owner.views import index, Profile
 
-# Create your tests here.
+from django.urls import reverse,resolve
+
+from django.test import SimpleTestCase
+
+from django.test.client import Client
+
+from .views import *
+
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_index_url(self):
+
+        url=reverse("Owner")
+
+        self.assertEquals(resolve(url).func,index)
+
+    
+
+    
+
+  
